d235de404b9476a8a5702dc28719189474559565
Loser <tacobellcommercial@gmail.com>
2025-03-29 08:24:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs")
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+  res.redirect("/home");
+})
+
+app.get("/home", (req, res)=>{
+  res.render("Home", {
+    title: "Yes: Reddit is potentially covering for extreme leftist communities"
+  });
+})
+
+app.get("/about", (req, res)=>{
+  res.render("About", {
+    title: "About this site"
+  })
+})
+
+app.get("/legal", (req, res)=>{
+  res.render("Legal", {
+    title: "Legal"
+  })
+})
+
+app.get("/support", (req, res)=>{
+  res.render("Support", {
+    title: "Support"
+  })
+})
+
+app.get("/contact", (req, res)=>{
+  res.render("Contact", {
+    title: "Contact"
+  })
+})
+
+
+app.get("*", (req, res)=>{
+  res.render("Error", {
+    title: "Error!"
+  })
+})
+
+
+
+app.listen(3000, ()=>{
+  console.log("Listening on port 3000");
+})
